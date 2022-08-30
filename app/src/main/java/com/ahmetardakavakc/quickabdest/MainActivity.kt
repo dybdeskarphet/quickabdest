48a9b558eb833aa42e62c68018abcf91caa790a2
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2022-08-30 22:51:16 +0300

    Change texts & add initial opening message

--- app/src/main/java/com/ahmetardakavakc/quickabdest/MainActivity.kt
+++ app/src/main/java/com/ahmetardakavakc/quickabdest/MainActivity.kt
@@ -7,6 +7,7 @@ import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import com.ahmetardakavakc.quickabdest.databinding.ActivityMainBinding
@@ -17,9 +18,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var githubIntent: Intent
     private lateinit var websiteIntent: Intent
+    private lateinit var creditsIntent: Intent
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var prefEditor: SharedPreferences.Editor
     private var darkMode = false
+    private var firstStart: Boolean? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,33 +32,66 @@ class MainActivity : AppCompatActivity() {
 
         githubIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/dybdeskarphet"))
         websiteIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://kavakci.dev"))
+        creditsIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://quickabdest.com"))
 
         sharedPreferences = getSharedPreferences("Prefs", MODE_PRIVATE)
         prefEditor = sharedPreferences.edit()
         darkMode = sharedPreferences.getBoolean("darkMode", false)
+        firstStart = sharedPreferences.getBoolean("firstStart", true)
 
         supportActionBar?.hide()
         checkDarkMode(view)
+        checkFirstStart()
 
     }
 
-
     fun clickPepe(view: View) {
-        binding.pepe.setImageResource(R.drawable.open)
-        binding.textView.text = "abdestlendin"
-        binding.pepe.isEnabled = false
+        binding.apply {
+            pepe.setImageResource(R.drawable.open)
+            emphaticText.text = "abdestlendin"
+            normalText.visibility = View.GONE
+            pepe.isEnabled = false
+        }
+
         mediaPlayer = MediaPlayer.create(this, R.raw.soundeffect)
 
         try {
             mediaPlayer.start()
 
             mediaPlayer.setOnCompletionListener {
-                binding.pepe.setImageResource(R.drawable.close)
-                binding.textView.text = "quickabdest"
-                binding.pepe.isEnabled = true
+                binding.apply {
+                    pepe.setImageResource(R.drawable.close)
+                    emphaticText.text = "tıkla"
+                    normalText.visibility = View.VISIBLE
+                    pepe.isEnabled = true
+                }
             }
+
         } catch (e: Exception) {
-            println(e)
+            e.printStackTrace()
+        }
+    }
+
+    private fun checkFirstStart() {
+        if (firstStart == true) {
+            firstStart = false
+            val builder = AlertDialog.Builder(this@MainActivity)
+            builder
+                .setMessage("Bu uygulamanın quickabdest.com geliştiricisi ile bir bağlantısı yoktur. Orijinal geliştiriciyi desteklemek için ")
+                .setPositiveButton("Geliştiriciyi Ziyaret Et"
+                ) { _, _ ->
+                    startActivity(creditsIntent)
+                    prefEditor.apply {
+                        putBoolean("firstStart", firstStart!!)
+                        apply()
+                    }
+                }
+                .setNegativeButton("Kapat") { _, _ ->
+                    prefEditor.apply {
+                        putBoolean("firstStart", firstStart!!)
+                        apply()
+                    }
+                }.show()
         }
     }
 
@@ -71,26 +107,43 @@ class MainActivity : AppCompatActivity() {
         if(darkMode) {
             WindowCompat.getInsetsController(window, view)?.isAppearanceLightStatusBars = false
 
-            binding.linearLayout.setBackgroundColor(Color.BLACK)
-            binding.darkToggle.setImageResource(R.drawable.ic_sun)
+            binding.apply {
+                mainLayout.setBackgroundColor(Color.BLACK)
+                darkToggle.setImageResource(R.drawable.ic_sun)
+                normalText.setTextColor(Color.WHITE)
+                appTitle.setTextColor(Color.WHITE)
+            }
 
-            window.navigationBarColor = Color.BLACK
-            window.statusBarColor = Color.BLACK
+            window.apply {
+                navigationBarColor = Color.BLACK
+                statusBarColor = Color.BLACK
+            }
+
         } else {
             WindowCompat.getInsetsController(window, view)?.isAppearanceLightStatusBars = true
 
-            binding.linearLayout.setBackgroundColor(Color.WHITE)
-            binding.darkToggle.setImageResource(R.drawable.ic_moon)
+            binding.apply {
+                mainLayout.setBackgroundColor(Color.WHITE)
+                darkToggle.setImageResource(R.drawable.ic_moon)
+                normalText.setTextColor(Color.BLACK)
+                appTitle.setTextColor(Color.BLACK)
+            }
 
-            window.navigationBarColor = Color.WHITE
-            window.statusBarColor = Color.WHITE
+            window.apply {
+                navigationBarColor = Color.WHITE
+                statusBarColor = Color.WHITE
+            }
         }
     }
 
     fun darkModeToggle(view: View) {
         darkMode = !darkMode
-        prefEditor.putBoolean("darkMode", darkMode)
-        prefEditor.apply()
+
+        prefEditor.apply {
+            putBoolean("darkMode", darkMode)
+            apply()
+        }
+
         checkDarkMode(view)
     }
 
@@ -111,7 +164,8 @@ class MainActivity : AppCompatActivity() {
     private fun stopPepe() {
         binding.apply {
             pepe.setImageResource(R.drawable.close)
-            textView.text = "quickabdest"
+            emphaticText.text = "tıkla"
+            normalText.visibility = View.VISIBLE
             pepe.isEnabled = true
         }
 
